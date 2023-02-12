4e6a750a00b95dbc9ea0231cb46822679d3aa744
Matt Ickstadt <mattico8@gmail.com>
2023-02-12 20:17:00 +0000

    diff: change to isize pos so we can follow the java logic exactly

--- src/java/diff.rs
+++ src/java/diff.rs
@@ -3,31 +3,36 @@ pub fn apply(orig: &str, diff: &str) -> Option<String> {
         return None;
     }
     let mut result = Vec::from_iter(orig.chars());
-    let mut pos = result.len();
+    let mut pos = (result.len() - 1) as isize;
     let mut chars = diff.chars();
     // TODO: replace with next_chunk when stable
     while let (Some(cmd), Some(param)) = (chars.next(), chars.next()) {
-        pos = pos.checked_sub(1)?;
         match cmd {
             '-' => {
                 assert!(param.is_ascii_lowercase());
-                let par_num = ((param as u8) - b'a') as usize;
-                pos = pos.checked_sub(par_num)?;
+                let par_num = ((param as u8) - b'a') as isize;
+                pos -= par_num;
             }
-            'R' => result[pos] = param,
+            'R' => *result.get_mut(usize::try_from(pos).ok()?)? = param,
             'D' => {
                 assert!(param.is_ascii_lowercase());
-                let par_num = ((param as u8) - b'a') as usize;
-                let o = pos;
-                pos = pos.checked_sub(par_num)?;
-                result.drain(pos..=o);
+                let par_num = ((param as u8) - b'a') as isize;
+                let o = usize::try_from(pos).ok()?;
+                pos -= par_num;
+                let s = usize::try_from(pos).ok()?;
+                result.drain(s..=o);
             }
             'I' => {
                 pos += 1;
-                result.insert(pos, param);
+                result.insert(usize::try_from(pos).ok()?, param);
             }
             _ => unreachable!(),
         }
+        pos -= 1;
     }
-    Some(result.into_iter().collect())
+    if result.is_empty() {
+        None
+    } else {
+        Some(result.into_iter().collect())
+    }
 }
